974d409faf2bce312630113338ef7078c057751f
y124 <64955721+y124@users.noreply.github.com>
2021-06-29 21:00:12 +1000

    Update pygameMERGER.py

--- code/pygameMERGER.py
+++ code/pygameMERGER.py
@@ -2,6 +2,31 @@ import pygame, os, math, random, json
 from PIL import Image, ImageDraw
 from PIL import ImagePath
 
+scaling = 1
+
+def sx(x):
+    s = scaling
+    m = 800
+    v = x - m
+    if v >= 0:
+        w = v*scaling + m*scaling
+    else:
+        w = x*scaling
+    return int(w)
+    
+def sy(x):
+    s = scaling
+    m = 450
+    v = x - m
+    if v > 0:
+        w = v*scaling + m*scaling
+    else:
+        w = x*scaling
+    return int(w)
+
+def s(x, y):
+    return (sx(x),sy(y))
+
 def primefactors(n):
        a = []
        while n % 2 == 0:
@@ -80,8 +105,8 @@ folder = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'Assets')
 pygame.init()
 def getFont(size):
     global folder
-    return pygame.font.Font(os.path.join(folder, 'font.ttf'), size)
-dis = pygame.display.set_mode((1600, 900))
+    return pygame.font.Font(os.path.join(folder, 'font.ttf'), int(size*scaling))
+dis = pygame.display.set_mode((int(1600*scaling), int(900*scaling)))
 pygame.display.set_caption('Merge Infinte (OPEN BETA)')
 pygame.display.flip()
 r = True
@@ -174,53 +199,79 @@ lmm = 0
 mval = 1
 merges = 0
 
-ftext = getFont(450).render('+', True, (0,255,127), (50,50,50))
-ftextRect = ftext.get_rect()
-ftextRect.center = (1150, 250)
+def inittext():
+    global ftext, ftextRect
+    global stext, stextRect
+    global aftext, aftextRect
+    global amtext, amtextRect
+    
+    global s1text, s1textRect
+    global s2text, s2textRect
+    global s3text, s3textRect
+    global ntext, ntextRect
+    
+    global autext, autextRect
+    global rtext, rtextRect
+    global m2text, m2textRect
+    global sttext, sttextRect
+    
+    global sctext, sctextRect
+    
+    ftext = getFont(450).render('+', True, (0,255,127), (50,50,50))
+    ftextRect = ftext.get_rect()
+    ftextRect.center = s(1150, 250)
 
-stext = getFont(160).render('sort', True, (0,255,127), (50,50,50))
-stextRect = stext.get_rect()
-stextRect.center = (1150, 800)
+    stext = getFont(160).render('sort', True, (0,255,127), (50,50,50))
+    stextRect = stext.get_rect()
+    stextRect.center = s(1150, 800)
 
-aftext = getFont(30).render(' Auto Plus Hax ', True, (0,255,127), (50,50,50))
-aftextRect = aftext.get_rect()
-aftextRect.center = (1500, 300)
+    aftext = getFont(30).render(' Auto Plus Hax ', True, (0,255,127), (50,50,50))
+    aftextRect = aftext.get_rect()
+    aftextRect.center = s(1500, 300)
 
-amtext = getFont(30).render(' Auto Merge Hax ', True, (0,255,127), (50,50,50))
-amtextRect = amtext.get_rect()
-amtextRect.center = (1486, 350)
+    amtext = getFont(30).render(' Auto Merge Hax ', True, (0,255,127), (50,50,50))
+    amtextRect = amtext.get_rect()
+    amtextRect.center = s(1486, 350)
 
-s1text = getFont(100).render('Save 1', True, (0,255,255), (50,50,50))
-s1textRect = s1text.get_rect()
-s1textRect.center = (400, 350)
+    s1text = getFont(100).render('Save 1', True, (0,255,255), (50,50,50))
+    s1textRect = s1text.get_rect()
+    s1textRect.center = s(400, 350)
 
-s2text = getFont(100).render('Save 2', True, (0,255,255), (50,50,50))
-s2textRect = s2text.get_rect()
-s2textRect.center = (800, 350)
+    s2text = getFont(100).render('Save 2', True, (0,255,255), (50,50,50))
+    s2textRect = s2text.get_rect()
+    s2textRect.center = s(800, 350)
 
-s3text = getFont(100).render('Save 3', True, (0,255,255), (50,50,50))
-s3textRect = s3text.get_rect()
-s3textRect.center = (1200, 350)
+    s3text = getFont(100).render('Save 3', True, (0,255,255), (50,50,50))
+    s3textRect = s3text.get_rect()
+    s3textRect.center = s(1200, 350)
+    
+    sctext = getFont(20).render('press + or - to change scaling', True, (50,50,0))
+    sctextRect = sctext.get_rect()
+    sctextRect.center = s(800, 450)
 
-sttext = getFont(200).render('Start', True, (127,0,127))
-sttextRect = sttext.get_rect()
-sttextRect.center = (800, 150)
+    sttext = getFont(200).render('Start', True, (127,0,127))
+    sttextRect = sttext.get_rect()
+    sttextRect.center = s(800, 150)
 
-ntext = getFont(100).render('Merge Infinte', True, (0,192,192))
-ntextRect = ntext.get_rect()
-ntextRect.center = (800, 500)
+    ntext = getFont(100).render('Merge Infinte', True, (0,192,192))
+    ntextRect = ntext.get_rect()
+    ntextRect.center = s(800, 500)
 
-autext = getFont(75).render('By y124', True, (0,128,128))
-autextRect = autext.get_rect()
-autextRect.center = (800, 650)
+    autext = getFont(75).render('By y124', True, (0,128,128))
+    autextRect = autext.get_rect()
+    autextRect.center = s(800, 650)
 
-rtext = getFont(30).render(' Randomize ', True, (0,255,127), (50,50,50))
-rtextRect = rtext.get_rect()
-rtextRect.center = (1520, 400)
+    rtext = getFont(30).render(' Randomize ', True, (0,255,127), (50,50,50))
+    rtextRect = rtext.get_rect()
+    rtextRect.center = s(1520, 400)
 
-m2text = getFont(15).render('Middle Click Shape to See in Fullscreen', True, (0,255,255))
-m2textRect = m2text.get_rect()
-m2textRect.center = (1460, 870)
+    m2text = getFont(15).render('Middle Click Shape to See in Fullscreen', True, (0,255,255))
+    m2textRect = m2text.get_rect()
+    m2textRect.center = s(1460, 870)
+    
+    dis = pygame.display.set_mode((int(1600*scaling), int(900*scaling)))
+
+inittext()
 
 clock = pygame.time.Clock()
 
@@ -234,6 +285,15 @@ while r:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             r = False
+        
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_MINUS:
+                if scaling > 0.25:
+                    scaling -= 0.25
+                inittext()
+            if event.key == pygame.K_EQUALS:
+                scaling += 0.25
+                inittext()
         
         if event.type == pygame.MOUSEBUTTONUP:
             if scene == 'START':
@@ -260,11 +320,11 @@ while r:
                     sav = 3
             
             if scene == 'MAIN' and tablet:
-                if pygame.mouse.get_pos()[0] < 900:
+                if pygame.mouse.get_pos()[0] < int(900*scaling):
                     print('[pygameMERGER.py] Click: Board')
                     if event.button == 1:
                         x, y = pygame.mouse.get_pos()
-                        x, y = int(x*(GridSize/900)), int(y*(GridSize/900))
+                        x, y = int(x*(GridSize/int(900*scaling))), int(y*(GridSize/int(900*scaling)))
                         end = False
                         
                         for i in range(len(mergeables)):
@@ -301,16 +361,16 @@ while r:
                 save(sav)
             except:
                 None == None
-            if event.button == 2 and scene == 'MAIN' and pygame.mouse.get_pos()[0] < 900:
+            if event.button == 2 and scene == 'MAIN' and pygame.mouse.get_pos()[0] < int(900*scaling):
                 try:
                     save(sav)
                 except:
                     None == None
                 x, y = pygame.mouse.get_pos()
-                x, y = int(x*(GridSize/900)), int(y*(GridSize/900))
+                x, y = int(x*(GridSize/int(900*scaling))), int(y*(GridSize/int(900*scaling)))
                 for m in mergeables:
                     if (m.x, m.y) == (x, y):
-                        path = f'{folder}/900_{m.val}.png'
+                        path = f'{folder}/{int(900*scaling)}_{m.val}.png'
                         try:
                             img = images[path]
                         except:
@@ -318,13 +378,13 @@ while r:
                                 images[path] = pygame.image.load(path)
                                 img = images[path]
                             except:
-                                command = f'--- 450 {m.val+2} {folder}/900_{m.val}.png {rgb2hex((25*(m.val%10+1),(25*(int(m.val/10)%10+1)),(25*(int(m.val/100)%10+1))))} '
+                                command = f'--- 450 {m.val+2} {folder}/{int(900*scaling)}_{m.val}.png {rgb2hex((25*(m.val%10+1),(25*(int(m.val/10)%10+1)),(25*(int(m.val/100)%10+1))))} '
                                 makeShape(command.split(' '))
                                 print('[pygameMERGER.py] Command: '+command)
                                 images[path] = pygame.image.load(path)
                                 img = images[path]
                         imgRect = img.get_rect()
-                        imgRect.center = (800, 450)
+                        imgRect.center = s(800, 450)
                         dis.blit(img, imgRect)
                         scene = 'BIG SHAPE'
                         bsv = m.val
@@ -388,10 +448,10 @@ while r:
                             mode = 'Number'
                         print('Mode:  ' + mode)
                                     
-                    if pygame.mouse.get_pos()[0] < 900:
+                    if pygame.mouse.get_pos()[0] < int(900*scaling):
                         print('[pygameMERGER.py] Click: Board')
                         x, y = pygame.mouse.get_pos()
-                        x, y = int(x*(GridSize/900)), int(y*(GridSize/900))
+                        x, y = int(x*(GridSize/int(900*scaling))), int(y*(GridSize/int(900*scaling)))
                         end = False
                         
                         for i in range(len(mergeables)):
@@ -403,7 +463,7 @@ while r:
                                 if (m.x, m.y) == (x, y) and hold[0] == False and m.shown == True:
                                     print('[pygameMERGER.py] Click: Shape')
                                     m.shown = False
-                                    qq = int(900/GridSize*0.9)
+                                    qq = int(int(900*scaling)/GridSize*0.9)
                                     path = f'{folder}/{qq}_{m.val}.png'
                                     try:
                                         img = images[path]
@@ -447,7 +507,7 @@ while r:
                 mval = m.val
         
             if m.shown:
-                qq = int(900/GridSize*0.9)
+                qq = int(int(900*scaling)/GridSize*0.9)
                 path = f'{folder}/{qq}_{m.val}.png'
                 try:
                     img = images[path]
@@ -465,29 +525,29 @@ while r:
                         images[path] = pygame.image.load(path)
                         img = images[path]
                 textRect = img.get_rect()
-                textRect.center = ((900 // GridSize)*(m.x+0.5), (900 // GridSize)*(m.y+0.5))
+                textRect.center = ((int(900*scaling) // GridSize)*(m.x+0.5), (int(900*scaling) // GridSize)*(m.y+0.5))
                 dis.blit(img, textRect)
                 
                 if mode == 'Number':
                     text = getFont(int(qq*0.35)).render(f'{str(m.val)}', True, (0,0,0))
                     textRect = text.get_rect()
-                    textRect.center = ((900 // GridSize)*(m.x+0.5), (900 // GridSize)*(m.y+0.5))
+                    textRect.center = ((int(900*scaling) // GridSize)*(m.x+0.5), (int(900*scaling) // GridSize)*(m.y+0.5))
                     dis.blit(text, textRect)
                     
                     text = getFont(int(qq*0.3)).render(f'{str(m.val)}', True, (255, 255, 255))
                     textRect = text.get_rect()
-                    textRect.center = ((900 // GridSize)*(m.x+0.5), (900 // GridSize)*(m.y+0.5))
+                    textRect.center = ((int(900*scaling) // GridSize)*(m.x+0.5), (int(900*scaling) // GridSize)*(m.y+0.5))
                     dis.blit(text, textRect)
                     
                 elif mode == 'Letter':
                     text = getFont(int(qq*0.35)).render(f'{i2b(m.val)}', True, (0,0,0))
                     textRect = text.get_rect()
-                    textRect.center = ((900 // GridSize)*(m.x+0.5), (900 // GridSize)*(m.y+0.5))
+                    textRect.center = ((int(900*scaling) // GridSize)*(m.x+0.5), (int(900*scaling) // GridSize)*(m.y+0.5))
                     dis.blit(text, textRect)
                     
                     text = getFont(int(qq*0.3)).render(f'{i2b(m.val)}', True, (255, 255, 255))
                     textRect = text.get_rect()
-                    textRect.center = ((900 // GridSize)*(m.x+0.5), (900 // GridSize)*(m.y+0.5))
+                    textRect.center = ((int(900*scaling) // GridSize)*(m.x+0.5), (int(900*scaling) // GridSize)*(m.y+0.5))
                     dis.blit(text, textRect)
     
     if scene == 'START':  
@@ -495,6 +555,7 @@ while r:
         dis.blit(s1text, s1textRect)
         dis.blit(s2text, s2textRect)
         dis.blit(s3text, s3textRect)
+        dis.blit(sctext, sctextRect)
         dis.blit(sttext, sttextRect)
         dis.blit(autext, autextRect)
     
@@ -503,17 +564,17 @@ while r:
         
         mtext = getFont(100).render(f'{str(bsv)}', True, (0,255,255))
         mtextRect = mtext.get_rect()
-        mtextRect.center = (150, 100)
+        mtextRect.center = s(150, 100)
         dis.blit(mtext, mtextRect) 
         
         mtext = getFont(100).render(f'{i2b(bsv)}', True, (0,255,255))
         mtextRect = mtext.get_rect()
-        mtextRect.center = (150, 200)
+        mtextRect.center = s(150, 200)
         dis.blit(mtext, mtextRect) 
         
         mtext = getFont(50).render(f'Left Click to Exit', True, (0,64,64))
         mtextRect = mtext.get_rect()
-        mtextRect.center = (1350, 850)
+        mtextRect.center = s(1350, 850)
         dis.blit(mtext, mtextRect) 
               
     if scene == 'MAIN':            
@@ -526,12 +587,12 @@ while r:
         
         mtext = getFont(25).render(f' Overlay Mode: {mode} ', True, (0,255,127), (50,50,50))
         mtextRect = mtext.get_rect()
-        mtextRect.center = (1465, 450)
+        mtextRect.center = s(1465, 450)
         dis.blit(mtext, mtextRect) 
         
         ttext = getFont(25).render(f" Tablet Mode: {['off', 'on '][tablet]} ", True, (0,255,127), (50,50,50))
         ttextRect = ttext.get_rect()
-        ttextRect.center = (1495, 250)
+        ttextRect.center = s(1495, 250)
         dis.blit(ttext, ttextRect) 
         
         if (((int(math.sqrt(mval+1)) + 1)**2) - 1) >= len(mergeables) and int(pygame.time.get_ticks()/100)%2 == 1 and af:
@@ -575,7 +636,7 @@ while r:
                     
         if hold[0] == True:
             m = hold[1]
-            qq = int(900/GridSize*0.9)
+            qq = int(int(900*scaling)/GridSize*0.9)
             path = f'{folder}/{qq}_{m.val}.png'
             try:
                 img = images[path]
@@ -618,11 +679,11 @@ while r:
         try:
             text = getFont(15).render(f'({str(1-(1/(merges**(1/(5-int(mval**(1/5)))))))[:8]} chance of higher block)', True, (0, 127, 127), (0, 0, 0))
             textRect = text.get_rect()
-            textRect.center = (1450, 80)
+            textRect.center = s(1450, 80)
             dis.blit(text, textRect)
             text = getFont(40).render(f'merges: {merges}', True, (0, 255, 255), (0,0,0))
             textRect = text.get_rect()
-            textRect.center = (1450, 35)
+            textRect.center = s(1450, 35)
             dis.blit(text, textRect)
         except ZeroDivisionError:
             None == None
